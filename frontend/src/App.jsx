8cf36e1e80296d35b4cb15cff7151dfc1391af85
seyawidumini <seyawidumini@gmail.com>
2026-04-25 15:08:57 +0530

    fix frontend map error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,37 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
   const [items, setItems] = useState([]);
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState("");
-  const [price, setPrice ] = useState("");
+  const [price, setPrice] = useState("");
 
-  const API = "https://item-manager-1-sn7g.onrender.com";
+  const API = "https://item-manager-1-sn7g.onrender.com/api/items";
 
-  // GET items
   const fetchItems = async () => {
     const res = await axios.get(API);
-    setItems(res.data);
+
+    if (Array.isArray(res.data)) {
+      setItems(res.data);
+    } else {
+      setItems([]);
+      console.log("Backend returned:", res.data);
+    }
   };
 
   useEffect(() => {
     fetchItems();
   }, []);
 
-  // ADD item
   const addItem = async () => {
     await axios.post(API, { name, quantity, price });
     setName("");
     setQuantity("");
+    setPrice("");
     fetchItems();
   };
 
-  // DELETE item
   const deleteItem = async (id) => {
     await axios.delete(`${API}/${id}`);
     fetchItems();
@@ -35,32 +39,33 @@ function App() {
 
   return (
     <div style={{ padding: "20px" }}>
-      <h2>Item Manager</h2>
+      <h1>Item Manager</h1>
 
       <input
         placeholder="Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+
       <input
         placeholder="Quantity"
         value={quantity}
         onChange={(e) => setQuantity(e.target.value)}
       />
 
-      <input 
+      <input
         placeholder="Price"
         value={price}
         onChange={(e) => setPrice(e.target.value)}
-        />
+      />
 
-      <button onClick={addItem}>Add</button>
+      <button onClick={addItem}>Add Item</button>
 
       <ul>
         {items.map((item) => (
           <li key={item._id}>
-            {item.name} - {item.quantity} -{item.price}
-            <button onClick={() => deleteItem(item._id)}>X</button>
+            {item.name} - {item.quantity} - Rs. {item.price}
+            <button onClick={() => deleteItem(item._id)}>Delete</button>
           </li>
         ))}
       </ul>
